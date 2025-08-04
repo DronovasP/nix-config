4790e3baf162206f114af6c57ff1b9a23a07bdbe
DronovasP <povilas.dronovas@gmail.com>
2025-08-04 10:12:37 +0300

    initial

---
+++ home-manager/home.nix
@@ -0,0 +1,53 @@
+# This is your home-manager configuration file
+# Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
+{
+  inputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  # You can import other home-manager modules here
+  imports = [
+    # If you want to use home-manager modules from other flakes (such as nix-colors):
+    # inputs.nix-colors.homeManagerModule
+
+    # You can also split up your configuration and import pieces of it here:
+    # ./nvim.nix
+  ];
+
+  nixpkgs = {
+    overlays = [];
+    config = {
+      allowUnfree = true;
+      allowUnfreePredicate = _: true;
+    };
+  };
+
+  home = {
+    username = "profke";
+    homeDirectory = "/home/profke";
+  };
+
+  programs.home-manager.enable = true;
+
+  programs.kitty.enable = true;
+  wayland.windowManager.hyprland.enable = true;
+
+
+  home.packages = with pkgs; [
+  xfce.mousepad
+  neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  wget
+  unzip
+# clipse
+  git
+  ];
+  
+
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "23.05";
+}
---
+++ nixos/configuration.nix
@@ -0,0 +1,72 @@
+# This is your system's configuration file.
+# Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
+{
+  inputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    ./hardware-configuration.nix
+  ];
+  nixpkgs = {
+    overlays = [];
+    config = {
+      allowUnfree = true;
+    };
+  };
+
+  nix = let
+    flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
+  in {
+    settings = {
+      experimental-features = "nix-command flakes";
+      # Opinionated: disable global registry
+      flake-registry = "";
+      nix-path = config.nix.nixPath;
+    };
+    # Opinionated: disable channels
+    channel.enable = false;
+
+    # Opinionated: make flake registry and nix path match flake inputs
+    registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
+    nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
+  };
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  networking.hostName = "nixos";
+  networking.networkmanager.enable = true;
+  time.timeZone = "Europe/Vilnius";
+  i18n.defaultLocale = "en_US.UTF-8";
+  
+  users.users = {
+    profke = {
+      isNormalUser = true;
+      description = "profke";
+      extraGroups = ["networkmanager" "wheel"];
+      openssh.authorizedKeys.keys = [
+      ];
+    };
+  };
+
+  programs.hyprland.enable = true;
+
+  programs.firefox.enable = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  kitty
+  xfce.mousepad
+  neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  wget
+  unzip
+# clipse
+  git
+  ];
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  system.stateVersion = "23.05";
+}
